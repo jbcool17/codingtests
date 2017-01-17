3604d590110baa0a80f06b9e24eff2afd2410c0b
John Brilla <jbrilla17@gmail.com>
2017-01-16 19:15:12 -0500

    update flatten array ruby

--- FlattenMultiDemArray/ruby.rb
+++ FlattenMultiDemArray/ruby.rb
@@ -1,4 +1,33 @@
-arr = [[1, 2],[3, 4, 5], [6, 7, 8, 9]]
-arr_two = [[1, 2],[3, [[1, 2],[3, 4, 5], [6, 7, 8, 9]], 5], [6, [[1, 2],[3, 4, 5], [6, 7, 8, 9]], 9]]
+# Method ONE
+arr_one = [[1,2,[3]],4]
+arr_two = [[1, 2, [322, 39, [3,22,[23,35,1],5,7554,]]], 4]
 
-arr.join('').split('').map(&:to_i)
+def flatten_array(arr)
+  # Create string from array with ','
+  # Split String into array based on ','
+  # Convert back to Integers
+  arr.join(',').split(',').map(&:to_i)
+end
+
+puts "OUTPUT: #{flatten_array arr_one}"
+puts "OUTPUT: #{flatten_array arr_two}"
+
+# Method TWO - Recursion - Method that calls itself
+
+def make_array_flat(arr, new_arr=[])
+
+    # Loop continues if Array found
+    # Loop will find integers and push them to new array
+    arr.each do |a|
+     if a.is_a? Array
+        make_array_flat(a, new_arr)
+     else
+        new_arr << a
+     end
+    end
+
+  new_arr
+end
+
+puts  "OUTPUT: #{make_array_flat arr_one}"
+puts  "OUTPUT: #{make_array_flat arr_two}"
--- Other/clutch.rb
+++ Other/clutch.rb
@@ -1,5 +1,6 @@
 # PROBLEM 5
 stock_prices_yesterday = [12,11,10,7,5,5,8,1,11]
+stock_prices_yesterday2 = [10,7,5,8,11,9]
 
 def get_max_profit(stock_prices_yesterday)
   arr = stock_prices_yesterday
@@ -26,3 +27,44 @@ def except_that(arr)
 end
 
 except_that arr
+
+# PROBLEM 7 - https://www.interviewcake.com/question/python/cake-thief
+cake_arrays = [[7,160],[3,90],[2,15]]
+capacity = 20
+
+def max_duffel_bag_value(cake_arrays, capacity)
+  values = {}
+  current_profit = 0
+  current_weight = 0
+  cake_arrays.each_with_index do |c, i|
+    p = capacity / c[0] * c[1]
+    w = capacity / c[0] * c[0]
+    current_profit = p if (p > current_profit )
+    current_weight = w if (w > current_weight && p >= current_profit )
+
+
+    # values[i] = [capacity / c[0] * c[1], capacity / c[0] * c[0]]
+  end
+
+  while current_weight < capacity do
+
+  end
+
+  values.select { |v| if v[0] }
+
+end
+
+total_profit = 0
+    rest_capacity = capacity
+
+    while rest_capacity > 0 do
+       most_profit = verify_profit cakes
+       cakes.delete(most_profit)
+       weight = most_profit[0]
+       profit = most_profit[1]
+       qt_to_carry =  (rest_capacity / weight).to_i
+       total_profit += profit * qt_to_carry
+       rest_capacity = rest_capacity % weight
+    end
+
+    total_profit
